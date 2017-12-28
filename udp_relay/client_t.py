7f1f8b494e3eaaefde7d59854ab7d26cc8dc1e30
meta <zycode277@gmail.com>
2017-12-28 13:41:11 +0800

    update

--- udp_relay/client_t.py
+++
@@ -1,9 +0,0 @@
-from client import client
-
-c = client(('0.0.0.0', 10002))
-c.sendto(b'hello',('127.0.0.1', 10000))
-
-while True:
-    data, address = c.recvfrom(4096)
-    if data:
-        print(str(address))
